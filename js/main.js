a1fb9f25aa0897aeb1ff118d1a20603cbfe703d6
hussein <artistpenman1515@gmail.com>
2025-08-28 14:54:28 +0330

    some challenges added

--- js/main.js
+++ js/main.js
@@ -25,3 +25,30 @@ const LargeNumber = (arr) => {
     return Math.max(...arr)
 }
 console.log(LargeNumber([332, 42, 423, 23423, 432, 3453434]));
+// remove first item from array
+const items = [1, 2, 3, 4, 5, 6]
+const removeItem = items.splice(1)
+console.log(removeItem);
+//callback function
+
+const callMyName = (name, callback) => {
+    callback(`Hello: ${name}`)
+}
+callMyName('Hussein', message => console.log(message))
+const obj = { name: "Hussein" }
+obj.age = 29
+console.log(obj);
+delete obj.age
+console.log(obj);
+console.log('options\n'.repeat(3));
+// sum of array number 
+const sumArray = (arr) => {
+    let sum = 0
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }
+    return sum;
+}
+console.log(sumArray
+    ([15, 34, 234, 342, 342, 342])
+);
